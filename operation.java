af8ccf33152a59dc3d0d11b031877b593d574042
halidirashidi <167415966+halidirashidi@users.noreply.github.com>
2024-04-23 08:11:04 +0300

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package condition_statement;
+
+/**
+ *
+ * @author user
+ */
+public class operation {
+   
+    int a,b;
+    
+    void add(){
+        int add=a+b;
+        System.out.print("sum is \n " +add);
+        
+    }
+      void sub(){
+        int sub=a-b;
+        System.out.print("difference  is \n " +sub);
+        
+    }
+        void prod(){
+        int prod=a*b;
+        System.out.print("product  is \n " +prod);
+        
+    }
+          void div(){
+        int div=a/b;
+        System.out.print("division is \n " +div);
+        
+    }
+    
+}
