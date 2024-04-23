992cde32c6225fe672e387c129b58393d34be139
halidirashidi <167415966+halidirashidi@users.noreply.github.com>
2024-04-23 08:10:16 +0300

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package condition_statement;
+
+/**
+ *
+ * @author user
+ */
+public class conditionals {
+   int d;
+    void iseven(){
+        
+        if(d%2==0){
+            System.out.print("number is even \n");
+        }
+    else { 
+            System.out.print("number is odd \n");
+        }
+        
+    }
+    
+}
